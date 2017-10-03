6681d1c37a89e1d57006afac58b9cfe398de97bf
shaklack02 <xtreme412@gmail.com>
2017-10-03 11:36:21 -0700

    Added Demo Pack + Clean classes

--- Source/MyProject/MyProjectCharacter.h
+++ Source/MyProject/MyProjectCharacter.h
@@ -32,19 +32,23 @@ public:
 
 	//Print out the all inventory 
 	UFUNCTION(BlueprintCallable)
-		void PrintInventory();
+	void PrintInventory();
 
 
 	//=========================================================================
 	/** Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
 	float BaseTurnRate;
 
 	/** Base look up/down rate, in deg/sec. Other scaling may affect final rate. */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
 	float BaseLookUpRate;
+
+
 	//Added by me for camera zoom
-	UPROPERTY(BlueprintReadWrite, Category = "Float Varibale")
+
+	//(BlueprintReadWrite, Category = "Float Varibale")
     float ZoomLength;
 private:
 	//Inventory Array 
@@ -102,9 +106,6 @@ public:
 	/** Returns CameraBoom subobject **/
 	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
 	/** Returns FollowCamera subobject **/
-	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
-	
-
-
+	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera;}
 };
 
